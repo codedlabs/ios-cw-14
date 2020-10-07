78ce4ee6478cf357d654a3131991488a798a1f5f
mariamalrashed-c57 <66645992+mariamalrashed-591@users.noreply.github.com>
2020-10-07 15:17:31 +0300

    done speedtest

--- speedtest-7/speedtest-7/ViewController.swift
+++ speedtest-7/speedtest-7/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    @IBAction func nextBtn(_ sender: Any) {
+        performSegue(withIdentifier: "nextPage", sender: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
